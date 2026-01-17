2694513e235b4c59d73a76db57aadcf548befc38
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2026-01-17 13:34:59 +0100

    Update emscripten bench signature

--- src/emscripten/misc/git-log-bench-signature.sh
+++ src/emscripten/misc/git-log-bench-signature.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+bench_file="$(dirname "$0")/../bench-signature.txt"
+
+if [ -f "$bench_file" ]; then
+  cat "$bench_file"
+  exit 0
+fi
+
 git log | grep "bench:" | head -n 1 | awk '{print $NF}'
